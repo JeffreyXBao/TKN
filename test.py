6e3cc667dc3d086baaaa646881da10fbcf0163d6
Jeffrey Bao <jb.lego2@gmail.com>
2020-11-20 23:55:42 -0600

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd
-import os, datetime
-import tensorflow as tf
-from tensorflow.keras.models import *
-from tensorflow.keras.layers import *
-print('Tensorflow version: {}'.format(tf.__version__))
-print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
